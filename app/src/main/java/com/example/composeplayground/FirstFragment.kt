a999c17f84f16ac3837784abf1c44919f389443b
Alex Pallister <alex.pallister@clearscore.com>
2022-08-12 14:04:31 +0100

    tidy playground

--- app/src/main/java/com/example/composeplayground/FirstFragment.kt
+++ app/src/main/java/com/example/composeplayground/FirstFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.composeplayground.compose.*
+import com.example.composeplayground.compose.BaseScaffold
 import com.example.composeplayground.databinding.FragmentFirstBinding
 
 /**
@@ -33,7 +33,7 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.composeView.setContent {
-            CardV5()
+            BaseScaffold()
         }
     }
 
---
+++ app/src/main/java/com/example/composeplayground/compose/BaseScaffold.kt
@@ -0,0 +1,12 @@
+package com.example.composeplayground.compose
+
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun BaseScaffold() {
+    Scaffold {
+    }
+}
